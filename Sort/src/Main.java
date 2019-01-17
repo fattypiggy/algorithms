58027763b46cc518df00ee20db431492ba3a8b83
William Jing <monkeykingisback@gmail.com>
2019-01-17 17:43:29 +0800

    update Sort.java and Main.java

--- Sort/src/Main.java
+++ Sort/src/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         int[] list = {1, 4, 3, 7, 10, 6, 2, 5, 8, 9};
+//      comment other lines to test the only sort algorithm.
+        SortUtil.selectSort(list);
         SortUtil.bubbleSort(list);
         SortUtil.insertSort(list);
-        SortUtil.selectSort(list);
         System.out.println(list);
     }
 }
--- Sort/src/com/williamjing/sort/SortUtil.java
+++ Sort/src/com/williamjing/sort/SortUtil.java
@@ -1,29 +1,72 @@
 package com.williamjing.sort;
 
-import java.util.Collections;
-import java.util.List;
-
 public class SortUtil {
 
+    /**
+     * select sort
+     * Time complexity: O(n²)
+     * @param list
+     */
     public static void selectSort(int[] list) {
-
+        for (int i = 0; i < list.length; i++) {
+            int min = i;
+            for (int j = i; j < list.length; j++) {
+                if (list[j] < list[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                swap(list, i, min);
+            }
+        }
     }
 
+    /**
+     * bubble sort
+     * Time complexity: O(n²)
+     * @param list
+     */
     public static void bubbleSort(int[] list) {
+        for (int i = 0; i < list.length - 1; i++) {
+            boolean flag = true;
+            for (int j = 0; j < list.length - i - 1; j++) {
+                if (list[j] > list[j + 1]) {
+                    swap(list, j, j + 1);
+                    flag = false;
+                }
+            }
+            if (flag) {
+                break;
+            }
+        }
 
     }
 
+    /**
+     * insert sort
+     * Time complexity: O(n²)
+     * @param list
+     */
     public static void insertSort(int[] list) {
-
+        for (int i = 1; i < list.length; i++) {
+            int j = i;
+            while (j > 0 && list[j] < list[j - 1]) {
+                swap(list, j - 1, j);
+                j--;
+            }
+        }
     }
 
-    private static final <T> void swap(T[] arrayList, int i, int j) {
-        T temp = arrayList[i];
+    /**
+     * swap the arrayList[i] and arrayList[j].
+     * @param arrayList
+     * @param i
+     * @param j
+     */
+    private static final void swap(int[] arrayList, int i, int j) {
+        int temp = arrayList[i];
         arrayList[i] = arrayList[j];
         arrayList[j] = temp;
-    }
-
-    private static final <T> void swap(List<T> list, int i, int j) {
-        Collections.swap(list, i, j);
+        System.out.printf("%d and %d swapped.\n", arrayList[j], arrayList[i]);
     }
 }
